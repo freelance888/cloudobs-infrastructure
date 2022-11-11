d2219c5cd60eb5fb6c9ab3a84241678a82e0b439
Akmal <aka.mukhsimov@gmail.com>
2022-11-11 20:07:12 +0000

    provision_delay

--- shared/scripts/functions/provision.sh
+++ shared/scripts/functions/provision.sh
@@ -5,6 +5,7 @@ function provision () {
   do
     echo " * Provisionion [ $lang ] | ${IPLANG[$lang]}"
     # ssh stream@${IPLANG[$lang]} nohup bash ./provision.sh $lang &
+    sleep 1
     ssh stream@${IPLANG[$lang]} nohup bash ./provision.sh $lang > /dev/null 2>&1 &
   done
 }
