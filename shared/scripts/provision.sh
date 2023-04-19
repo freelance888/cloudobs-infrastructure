f093fdec0033cea998897e8c6fe5ef74a53115bc
Akmal <aka.mukhsimov@gmail.com>
2023-04-19 11:49:11 +0000

    instance_service --user service

--- shared/scripts/provision.sh
+++ shared/scripts/provision.sh
@@ -20,6 +20,7 @@ rm -rf .ts3client .config/obs-studio
 mkdir -p /home/${username}/files
 tar -xf files.tar --warning=no-unknown-keyword
 sudo mv files/services/* /lib/systemd/system/
+sudo mv /lib/systemd/system/instance_service.service /lib/systemd/user/instance_service.service
 mv -f files/configs/.ts3client $HOME
 mv -f files/configs/obs-studio $HOME/.config/
 
@@ -31,17 +32,21 @@ cp ~/files/env .env
 sed -i -e '/LANG/d' .env
 echo "LANG=$lang" >> .env
 
-sudo systemctl daemon-reload
+pactl load-module module-null-sink sink_name=monitor_sink sink_properties=device.description=monitor_sink
+pactl load-module module-null-sink sink_name=obs_sink sink_properties=device.description=obs_sink
 
-sudo systemctl enable instance_service
-sudo systemctl enable gdrive_sync
+sudo systemctl daemon-reload
+systemctl --user daemon-reload
+
+systemctl --user enable instance_service
+#sudo systemctl enable gdrive_sync
 sudo systemctl enable obs
 sudo systemctl enable teamspeak
 
-sudo systemctl stop instance_service
-sudo systemctl stop gdrive_sync
-sudo systemctl start instance_service
-sudo systemctl start gdrive_sync
+systemctl --user stop instance_service
+#sudo systemctl stop gdrive_sync
+systemctl --user start instance_service
+#sudo systemctl start gdrive_sync
 
 # LAUNCH X SESSION
 sudo nohup Xvfb :1 -ac -screen 0 1024x768x24 &
@@ -63,9 +68,6 @@ DISPLAY=:1 nohup obs &
 # Run TeamSpeak
 cd $HOME/ts
 DISPLAY=:1 ./ts3client_runscript.sh "ts3server://ts.it-planeta.com?port=10335&channel=Translation/${lang}&nickname=${lang}_Synthetic_StreamListener" > /dev/null 2>&1 &
-
-pactl load-module module-null-sink sink_name=monitor_sink sink_properties=device.description=monitor_sink
-pactl load-module module-null-sink sink_name=obs_sink sink_properties=device.description=obs_sink
 
 rm /home/stream/PROVISION_STATUS
 echo "DONE" > /home/stream/PROVISION_STATUS
